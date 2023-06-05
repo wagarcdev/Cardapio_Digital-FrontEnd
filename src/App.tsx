b432aff7fff7d92ef1d3f397a9c7f11d8e57da72
Wagner Arcieri <wagner.arcieri@gmail.com>
2023-06-05 01:13:07 -0300

    final commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {Card} from "./components/card/card";
+import {useFoodData} from "./hooks/useFoodData";
+import {useState} from "react";
+import {CreateModal} from "./components/create-modal/create-modal";
+
+function App() {
+
+    const { data } = useFoodData();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(prevState => !prevState)
+    }
+
+  return (
+    <div className="container">
+        <h1>Cardápio</h1>
+        <div className="card-grid">
+            {data?.map(foodData =>
+                <Card
+                    price={foodData.price}
+                    title={foodData.title}
+                    image={foodData.image}
+                />
+            )}
+        </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>novo</button>
+    </div>
+  )
+}
+
+export default App
